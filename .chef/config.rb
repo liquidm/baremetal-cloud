bdb840fbda53b14c7f56c94f2187698707d7b13a
Hagen Rother <hagen.rother@liquidm.com>
2018-04-05 13:37:58 +0200

    remove chef, it doesn't belong here

--- .chef/config.rb
+++
@@ -1,9 +0,0 @@
-chef_zero.enabled true
-local_mode false
-chef_server_url   'http://127.0.0.1:8889'
-node_name         'stickywicket'
-client_key        "#{File.expand_path(File.join(File.dirname(__FILE__)))}/client.pem"
-
-cookbook_path [
-  "#{File.expand_path(File.join(File.dirname(__FILE__)))}/../cookbooks",
-]
