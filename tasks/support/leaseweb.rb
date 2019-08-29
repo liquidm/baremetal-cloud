736579955eb73376718ddd9da6dec0d778bc601f
Stuart McLean <stuart.mclean@liquidm.com>
2019-08-29 15:02:09 +0200

    leaseweb support: add cloud

--- tasks/support/leaseweb.rb
+++ tasks/support/leaseweb.rb
@@ -10,6 +10,13 @@ def leaseweb_init
       define_singleton_method :scan do |state|
         target_state = {}
 
+        # check for cloud servers
+        cloud_servers = account_api.getV2VirtualServers
+        if cloud_servers['errorMessage']
+          throw "#{account}: #{resp['errorMessage']}"
+        end
+        cloud = cloud_servers['virtualServers']
+
         # dedicated
         resp = account_api.getV2DedicatedServers
         if resp['errorMessage']
@@ -17,69 +24,68 @@ def leaseweb_init
         end
 
         metals=resp['servers']
-        unless metals
+        unless metals or cloud
           puts "no servers at leaseweb in #{account}?"
           next
         end
-        puts "#{metals.length} servers at Leaseweb in #{account}"
-        metals.each do |info|
-          host = baremetal_by_id(account, info['id'], state)
 
-          details = nil
-          begin
-            details = account_api.getV2DedicatedServer(info['id']) until details && !details['errorCode']
-          rescue
-            print 'e'
-            retry
-          end
+        if metals
+         puts "#{metals.length} servers at Leaseweb in #{account}"
+         metals.each do |info|
+           host = baremetal_by_id(account, info['id'], state)
 
-          # remove brand from chassis name
-          if details['specs'].key? 'brand'
-            details['specs']['chassis'].gsub!(/#{Regexp.escape(details['specs']['brand'])}/i, '')
-          end
+           details = nil
+           begin
+             details = account_api.getV2DedicatedServer(info['id']) until details && !details['errorCode']
+           rescue
+             print 'e'
+             retry
+           end
 
-          host[:isp][:info] = "#{details['specs']['brand'].strip} #{details['specs']['chassis'].strip} #{details['specs']['cpu']['type'].split(' ').last} #{details['specs']['ram']['size']}#{details['specs']['ram']['unit']} #{details['specs']['hdd'].map{|hdd| "#{hdd['amount']}*#{hdd['size']}#{hdd['unit']} #{hdd['type']}"}.join(',')}"
-          host[:isp][:dc] = info['location']['site']
-          host[:isp][:rack] = info['location']['rack']
-          host[:ipv4] = info['networkInterfaces']['public']['ip'].split('/').first rescue nil
-          naming_convention = "#{details['specs']['chassis'].gsub(/[^A-Za-z0-9]+/, '')}-#{info['location']['rack'].gsub(/[^A-Za-z0-9]+/, '')}-#{info['location']['site'].gsub(/[^0-9]+/, '')}-#{info['contract']['internalReference'].gsub(/[^A-Za-z0-9]+/, '') rescue "nr"}.#{info['location']['site'].gsub(/[^A-Za-z]+/, '')}".downcase
+           # remove brand from chassis name
+           if details['specs'].key? 'brand'
+             details['specs']['chassis'].gsub!(/#{Regexp.escape(details['specs']['brand'])}/i, '')
+           end
 
-          baremetal_id = baremetal_unique_id(naming_convention, host, state)
+           host[:isp][:info] = "#{details['specs']['brand'].strip} #{details['specs']['chassis'].strip} #{details['specs']['cpu']['type'].split(' ').last} #{details['specs']['ram']['size']}#{details['specs']['ram']['unit']} #{details['specs']['hdd'].map{|hdd| "#{hdd['amount']}*#{hdd['size']}#{hdd['unit']} #{hdd['type']}"}.join(',')}"
+           host[:isp][:dc] = info['location']['site']
+           host[:isp][:rack] = info['location']['rack']
+           host[:ipv4] = info['networkInterfaces']['public']['ip'].split('/').first rescue nil
+           naming_convention = "#{details['specs']['chassis'].gsub(/[^A-Za-z0-9]+/, '')}-#{info['location']['rack'].gsub(/[^A-Za-z0-9]+/, '')}-#{info['location']['site'].gsub(/[^0-9]+/, '')}-#{info['contract']['internalReference'].gsub(/[^A-Za-z0-9]+/, '') rescue "nr"}.#{info['location']['site'].gsub(/[^A-Za-z]+/, '')}".downcase
 
-          # add this machine to old state to avoid some weird edge cases
-          state[baremetal_id] = host
-          target_state[baremetal_id] = host
-        end
-        puts ''
+           baremetal_id = baremetal_unique_id(naming_convention, host, state)
 
-        # cloud
-        resp = account_api.getV2VirtualServers
-        if resp['errorMessage']
-          throw "#{account}: #{resp['errorMessage']}"
+           # add this machine to old state to avoid some weird edge cases
+           state[baremetal_id] = host
+           target_state[baremetal_id] = host
+         end
+          puts ''
         end
 
-        metals=resp['servers']
-        unless metals
+        # process cloud
+        unless cloud
           puts "no cloud servers at leaseweb in #{account}?"
-          next
-        end
-        puts "#{metals.length} cloud servers at Leaseweb in #{account}"
-        metals.each do |info|
-          host = baremetal_by_id(account, info['id'], state)
+        else
+          puts "#{cloud.length} cloud servers at Leaseweb in #{account}"
+          cloud.each do |info|
+            host = baremetal_by_id(account, info['id'], state)
 
-          hardware = info['hardware']
-          host[:isp][:info] = "#{hardware['cpu']['cores'].strip} core #{hardware['memory']['amount'].strip} #{hardware['memory']['unit'].strip} #{hardware['storage']['amount'].strip} #{hardware['storage']['unit'].strip} #{details['specs']['cpu']['type'].split(' ').last}"
-          host[:isp][:dc] = info['dataCenter']
-          info['ips'].each do |ip|
-            if ip['version'] && ip['type'] == 'PUBLIC' && !host[:ipv4]
-              host[:ipv4] = ip['ip']
+            hardware = info['hardware']
+            host[:isp][:info] = "#{hardware['cpu']['cores']} core #{hardware['memory']['amount']} #{hardware['memory']['unit'].strip} memory #{hardware['storage']['amount']} #{hardware['storage']['unit'].strip} storage"
+            host[:isp][:dc] = info['dataCenter']
+            info['ips'].each do |ip|
+              if ip['version'] == 4 && ip['visibility'] == 'PUBLIC'
+                host[:ipv4] = ip['ip']
+              end
             end
-          end
-          naming_convention = "vs-#{infor['dataCenter'].gsub(/[^0-9]+/, '')}".downcase
+            naming_convention = "vs-#{info['dataCenter'].gsub(/[^0-9]+/, '')}-#{info['id']}.#{info['dataCenter'].gsub(/[0-9-]+/, '')}".downcase
 
-          baremetal_id = baremetal_unique_id(naming_convention, host, state)
-          target_state[baremetal_id] = host
+            baremetal_id = baremetal_unique_id(naming_convention, host, state)
+            target_state[baremetal_id] = host
+          end
+          puts ''
         end
+
 
         target_state
       end
