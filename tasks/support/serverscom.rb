f462aab47e5ea2d3b8205a1b0a3188d455a8df72
Oleg Zhurko <oleg.zhurko@liquidm.com>
2020-03-20 20:32:58 +0100

    idrac rescue mode support

--- tasks/support/serverscom.rb
+++ tasks/support/serverscom.rb
@@ -1,3 +1,41 @@
+def scm_legacy_api_get_net_image(id, path)
+  uri = URI("https://portal.servers.com/rest/hosts/#{id}/network_configuration.iso")
+
+  req = Net::HTTP::Get.new(uri)
+  req['Content-Type'] = "application/json"
+  req['X-User-Email'] = $conf[:serverscom][:mail]
+  req['X-User-Token'] = $conf[:serverscom][:token]
+
+  Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+    resp = http.request(req)
+    open(path, "wb") do |file|
+      file.write(resp.body)
+    end
+  }
+end
+
+
+def scm_legacy_api(method, uri)
+  uri = URI(uri)
+
+  if method == "POST"
+    req = Net::HTTP::Post.new(uri)
+    req.body = "{}"
+  else
+    req = Net::HTTP::Get.new(uri)
+  end
+  req['X-Sc-Verification'] = $conf[:serverscom][:password]
+  req['Content-Type'] = "application/json"
+  req['X-User-Email'] = $conf[:serverscom][:mail]
+  req['X-User-Token'] = $conf[:serverscom][:token]
+
+  res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+    http.request(req)
+  }
+
+  return JSON.parse(res.body)['data']
+end
+
 def serverscom_init
   if $conf[:serverscom] && $conf[:serverscom][:mail]
     #h = serverscom::API.new($conf[:serverscom][:mail], $conf[:serverscom][:password])
@@ -8,17 +46,7 @@ def serverscom_init
       define_singleton_method :scan do |state|
         target_state = {}
 
-        uri = URI("https://portal.servers.com/rest/hosts")
-        req = Net::HTTP::Get.new(uri)
-        req['Content-Type'] = "application/json"
-        req['X-User-Email'] = $conf[:serverscom][:mail]
-        req['X-User-Token'] = $conf[:serverscom][:token]
-
-        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
-          http.request(req)
-        }
-
-        serverscom_servers = JSON.parse(res.body)['data']
+        serverscom_servers = scm_legacy_api("GET", "https://portal.servers.com/rest/hosts")
         puts "#{serverscom_servers.length} servers at servers.com"
 
         serverscom_servers.each do |s|
@@ -47,20 +75,44 @@ def serverscom_init
         id = host[:isp][:id]
         ip = host[:ipv4]
 
+        # idea:
+        # set idrac public POST https://portal.servers.com/rest/hosts/50857/features/oob_public_access/activate
+        # status check
+        # GET https://portal.servers.com/rest/hosts/50857/features
+        # wait for idrac
+        # boot rescue system through idrac
+        while (not (idrac_status = scm_legacy_api("GET", "https://portal.servers.com/rest/hosts/#{id}/features")
+          .select{|i| i['name'] == "oob_public_access" }
+          .first["state"]) == "activated")
+          if idrac_status == "deactivated"
+            # enabling idrac public access
+            scm_legacy_api("POST", "https://portal.servers.com/rest/hosts/#{id}/features/oob_public_access/activate")
+          end
+          # waiting till idrac will be present in public
+          puts "iDRAC still not in public mode (#{idrac_status})..."
+          sleep 20
+        end
+
+        idrac_credentials = scm_legacy_api("GET", "https://portal.servers.com/rest/hosts/#{id}/drac_credentials")
+        # servers.com not support DHCP, however they provide iso image with network settings for every server
+        scm_legacy_api_get_net_image(id, "/tmp/scm-net-#{id}.iso")
+
+        # prepare iso for boot
+
+        #puts %x{racadm -r #{idrac_credentials['ip']} -u #{idrac_credentials['login']} -p #{idrac_credentials['password']} getsysinfo}
+        #
+        # sequence to boot from iso
+        #puts %x{racadm -r #{idrac_credentials['ip']} -u #{idrac_credentials['login']} -p #{idrac_credentials['password']} set iDRAC.VirtualMedia.Attached Attached}
+        #puts %x{racadm -r #{idrac_credentials['ip']} -u #{idrac_credentials['login']} -p #{idrac_credentials['password']} remoteimage -c -l http://1.2.3.4/your-server-with-bootloader/rescue.iso}
+        #puts %x{racadm -r #{idrac_credentials['ip']} -u #{idrac_credentials['login']} -p #{idrac_credentials['password']} set iDRAC.VirtualMedia.BootOnce 1}
+        #puts %x{racadm -r #{idrac_credentials['ip']} -u #{idrac_credentials['login']} -p #{idrac_credentials['password']} set iDRAC.ServerBoot.FirstBootDevice VCD-DVD}
+        #puts %x{racadm -r #{idrac_credentials['ip']} -u #{idrac_credentials['login']} -p #{idrac_credentials['password']} serveraction powercycle}
+
         fingerprint = %x{ssh-keygen -E md5 -lf #{PRIVATE_SSH_KEY}.pub}.split[1].gsub(/^.*?\:/,'')
         puts "putting #{id} into rescue, ssh fingerprint #{fingerprint}"
 
-        uri = URI("https://portal.servers.com/rest/hosts/#{id}/enter_rescue_mode")
-        req = Net::HTTP::Post.new(uri)
-        req['Content-Type'] = "application/json"
-        req['X-User-Email'] = $conf[:serverscom][:mail]
-        req['X-User-Token'] = $conf[:serverscom][:token]
-        req['X-Sc-Verification'] = $conf[:serverscom][:password]
-        req.body = "{}"
-
-        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
-          http.request(req)
-        }
+        # command for putting to servers.com based rescuesystem to rescue mode
+        # scm_legacy_api("POST", "https://portal.servers.com/rest/hosts/#{id}/enter_rescue_mode")
 
         wait_for_ssh(ip)
       end
